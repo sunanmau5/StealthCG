5fb393ec37438b70ab6a712ed80975ab96d9336e
sunanmau5 <sunan.regi@gmail.com>
2021-06-06 13:43:32 +0200

    feat: add minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
