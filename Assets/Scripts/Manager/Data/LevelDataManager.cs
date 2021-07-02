7a39cb8eb2076b0a88801360186ff58abd324c82
Louis Andrew <louis.andrew@share-now.com>
2021-07-02 12:04:38 +0200

    feat: add `LevelData` to enable progress saving

---
+++ Assets/Scripts/Manager/Data/LevelDataManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class LevelDataManager : MonoBehaviour
+{
+    private LevelData levelData;
+    private BinaryFormatter formatter = new BinaryFormatter();
+    private string path;
+    void Start()
+    {
+        path = Application.persistentDataPath + "/level.data";
+        LoadLevelData();
+    }
+
+    // call to get last level data (call on `StartScene`)
+    public int GetLastLevel()
+    {
+        return levelData.Level;
+    }
+
+    public void SetLastLevel(int lastLevel)
+    {
+        levelData.Level = lastLevel;
+        SaveLevelData();
+    }
+
+    void SaveLevelData()
+    {
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, levelData);
+        stream.Close();
+    }
+
+    public void LoadLevelData()
+    {
+        if (File.Exists(path))
+        {
+            FileStream stream = new FileStream(path, FileMode.Open);
+            levelData = formatter.Deserialize(stream) as LevelData;
+            stream.Close();
+        }
+        else
+        {
+            levelData = new LevelData();
+        }
+    }
+}
